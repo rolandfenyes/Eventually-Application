116057a091b1e83f5f10975d3c5ea895fcb3a7c5
rolandfenyes <ftendalan@gmail.com>
2020-02-20 20:32:33 +0100

    Initial Commit

---
+++ Eventually/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Eventually
+//
+//  Created by Fényes Roland on 2020. 02. 20..
+//  Copyright © 2020. Fényes Roland. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Eventually/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Eventually
+//
+//  Created by Fényes Roland on 2020. 02. 20..
+//  Copyright © 2020. Fényes Roland. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
