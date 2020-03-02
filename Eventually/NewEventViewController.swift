0b17ffd2921d8b16f65062664cb733f0c25d3132
rolandfenyes <ftendalan@gmail.com>
2020-03-02 22:39:42 +0100

    Publicity picker is done.

--- Eventually/NewEventViewController.swift
+++ Eventually/NewEventViewController.swift
@@ -8,25 +8,59 @@
 
 import UIKit
 
-class NewEventViewController: UIViewController {
+class NewEventViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
     @IBOutlet weak var dateInput: UITextField!
-    private var datePicker : UIDatePicker?
+    private var datePicker: UIDatePicker?
+    
+    @IBOutlet weak var publicityInput: UITextField!
+    private var pubPicker: UIPickerView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
         DatePicking()
+        PublicityPicking()
         
         // Do any additional setup after loading the view.
     }
+    
+    //MARK: - Publicity Picker
+    
+    private let dataSourceOfPubPicker = ["Publikus", "Privát", "Közeli ismerősök"]
+    
+    func PublicityPicking() {
+        pubPicker = UIPickerView()
+        pubPicker?.delegate = self
+        pubPicker?.dataSource = self
+        publicityInput.inputView = pubPicker
+        SetUpDoneButton(pickerType: "PubPicker")
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dataSourceOfPubPicker.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return dataSourceOfPubPicker[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        publicityInput.text = dataSourceOfPubPicker[row]
+    }
+    
+    //MARK: - Date Picker
     
     func DatePicking(){
         datePicker = UIDatePicker()
         datePicker?.datePickerMode = .dateAndTime
         datePicker?.addTarget(self, action: #selector(NewEventViewController.DateChanged(datePicker:)), for: .valueChanged)
         dateInput.inputView = datePicker
-        SetUpDoneButton()
+        SetUpDoneButton(pickerType: "DatePicker")
     }
     
     @objc func DateChanged(datePicker: UIDatePicker) {
@@ -37,24 +71,31 @@ class NewEventViewController: UIViewController {
         dateInput.text = dateFormatter.string(from: datePicker.date)
     }
     
-    func SetUpDoneButton() {
+    // MARK: - Done Button
+    
+    func SetUpDoneButton(pickerType: String) {
         let toolBar = UIToolbar()
         toolBar.barStyle = .default
         toolBar.isTranslucent = true
         
         let space = UIBarButtonItem(barButtonSystemItem: .flexibleSpace, target: nil, action: nil)
-        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(DoneDatePickerPressed(sender: )))
+        let doneButton = UIBarButtonItem(title: "Done", style: .done, target: self, action: #selector(DoneButtonPressed(sender: )))
         doneButton.tintColor = UIColor.black
         toolBar.setItems([space, doneButton], animated: false)
         toolBar.sizeToFit()
-        AddDoneButtonToDatePicker(doneButton: toolBar)
+        AddDoneButton(doneButton: toolBar, pickerType: pickerType)
     }
     
-    func AddDoneButtonToDatePicker(doneButton : UIToolbar) {
-        dateInput.inputAccessoryView = doneButton
+    func AddDoneButton(doneButton : UIToolbar, pickerType: String) {
+        switch (pickerType) {
+        case "DatePicker": dateInput.inputAccessoryView = doneButton
+        case "PubPicker": publicityInput.inputAccessoryView = doneButton
+        default: break
+        }
+        
     }
     
-    @objc func DoneDatePickerPressed(sender: UIBarButtonItem) {
+    @objc func DoneButtonPressed(sender: UIBarButtonItem) {
         self.view.endEditing(true)
     }
 
