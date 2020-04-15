2e7733d7ad6890c27dcee394ada88eec75c51e95
rolandfenyes <ftendalan@gmail.com>
2020-04-15 09:56:06 +0200

    variables and buttons added to pevc

--- Eventually/ViewControllers/ProfileEditingViewController.swift
+++ Eventually/ViewControllers/ProfileEditingViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class ProfileEditingViewController: UIViewController {
 
+    //MARK: - Variables
+    
+    @IBOutlet weak var profileName: UITextField!
+    @IBOutlet weak var birthDate: UITextField!
+    @IBOutlet weak var emailAddress: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var profileDescription: UITextField!
+    
+    //MARK: - Main
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    //MARK: - Buttons
+    
+    @IBAction func changeProfilePicture(_ sender: UIButton) {
     }
-    */
-
-}
+    
+    @IBAction func cancel(_ sender: UIButton) {
+    }
+    
+    @IBAction func save(_ sender: UIButton) {
+    }
+    
+} //end of the class
