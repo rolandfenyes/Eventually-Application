671ba5faae506f0199e4f7a3eb20e70b8de6cf24
rolandfenyes <ftendalan@gmail.com>
2020-04-15 09:50:58 +0200

    ViewController class attached to the ProfileEditingViewController

---
+++ Eventually/ViewControllers/ProfileEditingViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileEditingViewController.swift
+//  Eventually
+//
+//  Created by Fényes Roland on 2020. 04. 15..
+//  Copyright © 2020. Fényes Roland. All rights reserved.
+//
+
+import UIKit
+
+class ProfileEditingViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
